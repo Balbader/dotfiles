0d215be222b545f085c25328949852cda7b32862
Balbader <15971200+Balbader@users.noreply.github.com>
2023-03-20 12:51:01 +0100

    update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -149,74 +149,6 @@ require('mind').setup( {
 })
 
 
---------------
--- MATERIAL --
---------------
-require('material').setup({
-    styles = { -- Give comments style such as bold, italic, underline etc.
-        comments = { italic = true },
-        strings = { --[[ bold = true ]] },
-        keywords = { italic = true },
-        functions = { bold = true },
-        variables = {},
-        operators = {},
-        types = { italic = true },
-    },
-
-    plugins = { -- Uncomment the plugins that you use to highlight them
-        -- Available plugins:
-        "dap",
-        "indent-blankline",
-        "nvim-cmp",
-        "nvim-navic",
-        "nvim-tree",
-        "telescope",
-        "trouble",
-        "which-key",
-    },
-
-	contrast = {
-        terminal = true, -- Enable contrast for the built-in terminal
-        sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-        floating_windows = false, -- Enable contrast for floating windows
-        cursor_line = false, -- Enable darker background for the cursor line
-        non_current_windows = false, -- Enable darker background for non-current windows
-        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
-    },
-
-    disable = {
-        colored_cursor = false, -- Disable the colored cursor
-        borders = false, -- Disable borders between verticaly split windows
-        -- background = true, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-        background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-        term_colors = false, -- Prevent the theme from setting terminal colors
-        eob_lines = false -- Hide the end-of-buffer lines
-    },
-
-    high_visibility = {
-        lighter = true, -- Enable higher contrast text for lighter style
-        darker = true -- Enable higher contrast text for darker style
-    },
-
-    lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
-    async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
-	custom_colors = function(colors)
-		-- colors.syntax.keyword = "#F3701D"
-		-- colors.syntax.fn = "#40BF54"
-		-- colors.syntax.variable = "#F3701D"
-	end
-	, -- If you want to everride the default colors, set this to a function
-})
-
--- vim.g.material_style = "deep ocean"
--- vim.g.material_style = "darker"
--- vim.g.material_style = "lighter"
--- vim.g.material_style = "oceanic"
--- vim.g.material_style = "palenight"
-
--- lvim.colorscheme = "material"
-
-
 -------------
 -- ONEDARK --
 -------------
@@ -341,7 +273,6 @@ lvim.plugins = {
 
 -- extra colorscheme
 	{ "Mofiqul/dracula.nvim" },
-	{ "marko-cerovac/material.nvim" },
 	{ "fenetikm/falcon" },
 
 
